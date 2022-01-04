9f92be84d2176f6d34828583e8d622fae420f1d7
Jairo Panetta <panetta@exahost.exadomain>
2022-01-03 21:34:06 -0300

    still removing output colors and blinks from compilation; changes made at BRAMS-6.0/build/paths.mk are  documented by comments starting with string 'JP:'

--- build/paths.mk
+++ build/paths.mk
@@ -114,17 +114,20 @@ EXE     =$(BIN_DIR)/brams-$(BRAMS_VERSION)
 LIBMODEL=$(BIN_DIR)/brams-$(BRAMS_VERSION).a
 
 ## TERMINAL COLOURS PARAMETERS ##
-NO_COLOUR   = "\033[0m"
-BOLD        = "\033[1m"
-RED         = "\033[0;31m"
-BOLD_RED    = "\033[1;31m"
-GREEN       = "\033[0;32m"
-BOLD_GREEN  = "\033[1;32m"
-YELLOW      = "\033[0;33m"
-BOLD_YELLOW = "\033[1;33m"
-BLUE        = "\033[0;34m"
-BOLD_BLUE   = "\033[1;34m"
-PURPLE      = "\033[0;35m"
-BOLD_PURPLE = "\033[1;35m"
-CYAN        = "\033[0;36m"
-BOLD_CYAN   = "\033[1;36m"
+#
+#  JP: removing colors from compilation output
+#
+#NO_COLOUR   = "\033[0m"
+#BOLD        = "\033[1m"
+#RED         = "\033[0;31m"
+#BOLD_RED    = "\033[1;31m"
+#GREEN       = "\033[0;32m"
+#BOLD_GREEN  = "\033[1;32m"
+#YELLOW      = "\033[0;33m"
+#BOLD_YELLOW = "\033[1;33m"
+#BLUE        = "\033[0;34m"
+#BOLD_BLUE   = "\033[1;34m"
+#PURPLE      = "\033[0;35m"
+#BOLD_PURPLE = "\033[1;35m"
+#CYAN        = "\033[0;36m"
+#BOLD_CYAN   = "\033[1;36m"
