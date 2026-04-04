e6a6233a6a52ee171bb56b36dac33090dc0863a7
Mateus FFreitas <mateus.ffreitas@hotmail.com>
2026-04-04 17:08:40 -0300

    Adiciona checagem de GNU 14+ para usar flags de compilação apropriadas

--- build/utils.mk
+++ build/utils.mk
@@ -4,4 +4,4 @@ export GCC_VERSION := $(shell gcc -dumpfullversion)
 export GCC_MAJOR_VERSION := $(firstword $(subst ., , $(GCC_VERSION)))
 export GCC_VERSION_INT := $(call GCC_VER_TO_INT, $(GCC_VERSION))
 export GCC_VER_GTE10 := $(shell [ $(GCC_VERSION_INT) -ge 100000 ] || echo 0 )
-
+export GCC_VER_GTE14 := $(shell [ $(GCC_VERSION_INT) -ge 140000 ] || echo 0 )
