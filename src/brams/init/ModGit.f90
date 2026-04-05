d0658a1b0cb9c617627f9ff0e9031073ffe2f546
Mateus FFreitas <mateus.ffreitas@hotmail.com>
2026-04-05 11:35:40 -0300

    Atualiza ModGit

--- src/brams/init/ModGit.f90
+++ src/brams/init/ModGit.f90
@@ -1,6 +1,6 @@
 module ModGit
-character(len=*), parameter :: GitBranch="master"
-character(len=*), parameter :: GitHash="fbc3acb"
-character(len=*), parameter :: GitDate="2025-10-24 @ 23:22:36-0300"
-character(len=*), parameter :: CompDate="2025-11-07 @ 16:43:09-0300"
+character(len=*), parameter :: GitBranch="main"
+character(len=*), parameter :: GitHash="3951334"
+character(len=*), parameter :: GitDate="2026-04-05 @ 11:33:31-0300"
+character(len=*), parameter :: CompDate="2026-04-05 @ 11:35:01-0300"
 end module ModGit
